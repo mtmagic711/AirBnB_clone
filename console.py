c0972bfdb2b26ee01648dc59587083a984698f2c
mtmagic <mtmagic711@gmail.com>
2024-03-27 03:07:44 +0000

    console task

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module is the module Cmd"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """The class module"""
+
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """the kill signal (ctrl + D)"""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """to quit the console"""
+        print()
+        return True
+
+    def empty(self, line):
+        """empty line handling"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
